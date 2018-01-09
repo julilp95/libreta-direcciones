b259dac1c963c99961d113602ff9b793b1c00d76
Julian Lorenzo <>
2018-01-09 17:07:07 +0100

    He añadido LibretaDirecciones.java

---
+++ src/controler/LibretaDirecciones.java
@@ -0,0 +1,77 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controler;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author dam
+ */
+public class LibretaDirecciones extends Application {
+    
+    private Stage escenarioPrincipal;
+    private BorderPane layoutPrincipal;
+    private AnchorPane listaPersona;
+    
+    @Override
+    public void start(Stage escenarioPrincipal) {
+        
+        this.escenarioPrincipal = escenarioPrincipal;
+        
+        //Establezco el titulo
+        this.escenarioPrincipal.setTitle("Libreta de Direcciones");
+        
+        //Inicializa el layoutPrincipal
+        initLayoutPrincipal();
+        
+        //Muestra la vistaPersona
+        muestraListaPersona();
+        
+    }
+    
+    public void initLayoutPrincipal(){
+        
+        //Cargo el layout principal a partir de la vistaPrincipal.fxml
+        FXMLLoader loader = new FXMLLoader();
+        URL location = LibretaDirecciones.class.getResource("../view/VistaPrincipal.fxml");
+        loader.setLocation(location);
+        try {
+            layoutPrincipal = loader.load();
+        } catch (IOException ex) {
+            Logger.getLogger(LibretaDirecciones.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        //Cargo y muestro la escena que contiene ese layout principal
+        Scene escena = new Scene(layoutPrincipal);
+        escenarioPrincipal.setScene(escena);
+        escenarioPrincipal.show();
+        
+    }
+    
+    public void muestraListaPersona(){
+        
+        
+        
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
